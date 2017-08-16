6944e4722522dc6a07d3b4bf24842c0d71325181
sohail111 <sohailmalik210@gmail.com>
2017-08-16 15:34:41 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sohailmalik/myfirstsummerproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sohailmalik.myfirstsummerproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MADA", "onCreate: we are in the on create method");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
